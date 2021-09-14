12755a1277f7051b14c4cc1dbf3e422a2f11ef41
João Gabriel <main.joaogabrielsg@gmail.com>
2021-09-14 16:01:34 -0300

    add theme for styled components

---
+++ src/@types/styled.d.ts
@@ -0,0 +1,13 @@
+declare module 'styled-components' {
+  export interface DefaultTheme {
+    colors: {
+      black: string
+      lightBlack: string
+      gray: string
+      white: string
+      primary: string
+    }
+  }
+}
+
+export {}
---
+++ src/app.tsx
@@ -0,0 +1,12 @@
+import styled, { css } from 'styled-components/macro'
+
+function App () {
+  return <Title>App</Title>
+}
+
+const Title = styled.h1`${({ theme }) => css`
+  background: ${theme.colors.black};
+  color: ${theme.colors.primary};
+`}`
+
+export { App }
