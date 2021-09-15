6ebe8790d793a55684efeeb3452aebaeb353e3b8
João Gabriel <main.joaogabrielsg@gmail.com>
2021-09-15 10:02:13 -0300

    ✨ feat: add mock data

---
+++ src/mocks/files.ts
@@ -0,0 +1,45 @@
+type File = {
+  id: string
+  name: string
+  content: string
+  active: boolean
+  status: 'editing' |'saving' | 'saved'
+}
+
+export const files: File[] = [
+  {
+    id: 'jahklfsajfhlk12rt90h120t03hf',
+    name: 'README',
+    content: '## Markee App',
+    active: false,
+    status: 'saved',
+  },
+  {
+    id: 'ajfo19rj1fp1ojf1-u901f',
+    name: 'CONTRIBUTING',
+    content: '## Tutorial on how to contribute to the project',
+    active: false,
+    status: 'saved',
+  },
+  {
+    id: 'j1ropj1onf190fh111-14401j-1341f',
+    name: 'LICENSE',
+    content: '## MIT License',
+    active: false,
+    status: 'saved',
+  },
+  {
+    id: 'jahklfsajfhlk12rt90h120t03hf',
+    name: 'Links',
+    content: '## Some important links',
+    active: false,
+    status: 'saved',
+  },
+  {
+    id: 'jahklfsajfhlk12rt90h120t03hf',
+    name: 'roadmap',
+    content: '## Bootcamp Brainn Co.',
+    active: true,
+    status: 'editing',
+  },
+]
