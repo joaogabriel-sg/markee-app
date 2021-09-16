de3000470faa0197bae0d972486605bee70c0b7b
João Gabriel <main.joaogabrielsg@gmail.com>
2021-09-16 14:31:24 -0300

    ✨ feat: add dynamic files in aside component Add dynamic file creation in aside component Delete files mocks

--- src/components/aside/index.tsx
+++ src/components/aside/index.tsx
@@ -1,20 +1,43 @@
-import { File } from 'components/aside/file'
+import { useState } from 'react'
+import { v4 as uuidv4 } from 'uuid'
 
-import * as S from './styles'
+import { File as TypeFile } from 'resources/types/file.type'
+
+import { File } from 'components/aside/file'
 
 import logoImg from 'resources/assets/logo.svg'
 import plusIcon from 'resources/assets/plus-symbol.svg'
 
-import { files } from 'resources/mocks/files'
+import * as S from './styles'
 
 export function Aside () {
+  const [files, setFiles] = useState<TypeFile[]>([])
+
+  const handleAddNewFile = () => {
+    const newFile: TypeFile = {
+      id: uuidv4(),
+      name: 'Sem título',
+      content: '',
+      active: true,
+      status: 'saved',
+    }
+
+    setFiles((prevFiles) => {
+      const prevFilesDisabled = prevFiles.map(
+        (prevFile) => ({ ...prevFile, active: false }),
+      )
+
+      return [...prevFilesDisabled, newFile]
+    })
+  }
+
   return (
     <S.Container>
-      <S.LogoImg src={logoImg} alt='Markee App' />
+      <S.LogoImg src={logoImg} alt='Markee App' title='Markee App' />
 
       <S.Title>Arquivos</S.Title>
 
-      <S.AddFileButton>
+      <S.AddFileButton type='button' onClick={handleAddNewFile}>
         <S.PlusIcon src={plusIcon} alt='Adicionar arquivo' />
         Adicionar arquivo
       </S.AddFileButton>
