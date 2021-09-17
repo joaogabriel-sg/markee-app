e1cb7b9e960b7143a8bc3151d75e52c53f202b5c
João Gabriel <main.joaogabrielsg@gmail.com>
2021-09-17 19:51:04 -0300

    ✨ feat: add useFiles custom hook

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,4 @@
-import { useEffect, useRef, useState } from 'react'
-import { v4 as uuidv4 } from 'uuid'
-
-import { File } from 'resources/types/file.type'
+import { useFiles } from 'hooks/useFiles'
 
 import { Sidebar } from './components/sidebar'
 import { ContentArea } from 'components/content-area'
@@ -9,86 +6,16 @@ import { ContentArea } from 'components/content-area'
 import * as S from './styles/app-styles'
 
 function App () {
-  const [files, setFiles] = useState<File[]>([])
-  const [currentFile, setCurrentFile] = useState<File>({} as File)
-  const inputRef = useRef<HTMLInputElement>(null)
-
-  useEffect(() => {
-    setFiles((prevFiles) => prevFiles.map(
-      (prevFile) => prevFile.id === currentFile.id
-        ? currentFile
-        : { ...prevFile, active: false },
-    ))
-
-    if (currentFile.status !== 'saved') {
-      const savingStatusTimeoutId = setTimeout(() => {
-        setCurrentFile((prevCurrentFile) => ({
-          ...prevCurrentFile,
-          status: 'saving',
-        }))
-
-        const savedStatusTimeoutId = setTimeout(() => {
-          setCurrentFile((prevCurrentFile) => ({
-            ...prevCurrentFile,
-            status: 'saved',
-          }))
-          clearTimeout(savedStatusTimeoutId)
-        }, 300)
-      }, 300)
-
-      return () => clearTimeout(savingStatusTimeoutId)
-    }
-  }, [currentFile])
-
-  const handleAddNewFile = () => {
-    const newFile: File = {
-      id: uuidv4(),
-      name: 'Sem título',
-      content: '',
-      active: true,
-      status: 'saved',
-    }
-
-    inputRef.current?.focus()
-    setFiles((prevFiles) => {
-      const prevFilesDisabled = prevFiles.map((prevFile) => ({
-        ...prevFile,
-        active: false,
-      }))
-      return [...prevFilesDisabled, newFile]
-    })
-    setCurrentFile(newFile)
-  }
-
-  const changeCurrentFilename = (newFilename: string) => {
-    setCurrentFile((prevCurrentFile) => ({
-      ...prevCurrentFile,
-      name: newFilename,
-      status: 'editing',
-    }))
-  }
-
-  const changeCurrentContent = (newContent: string) => {
-    setCurrentFile((prevCurrentFile) => ({
-      ...prevCurrentFile,
-      content: newContent,
-      status: 'editing',
-    }))
-  }
-
-  const changeFileById = (id: string) => {
-    const newCurrentFile = files.find((file) => file.id === id)
-    if (!newCurrentFile) return
-
-    inputRef.current?.focus()
-    setCurrentFile({ ...newCurrentFile, active: true })
-  }
-
-  const deleteFileById = (id: string) => {
-    setFiles((prevFiles) => prevFiles.filter(
-      (prevFile) => prevFile.id !== id,
-    ))
-  }
+  const {
+    files,
+    currentFile,
+    inputRef,
+    handleAddNewFile,
+    changeCurrentFilename,
+    changeCurrentContent,
+    changeFileById,
+    deleteFileById,
+  } = useFiles()
 
   return (
     <S.Container>
---
+++ src/hooks/useFiles.ts
@@ -0,0 +1,98 @@
+import { useEffect, useRef, useState } from 'react'
+import { v4 as uuidv4 } from 'uuid'
+
+import { File } from 'resources/types/file.type'
+
+export function useFiles () {
+  const [files, setFiles] = useState<File[]>([])
+  const [currentFile, setCurrentFile] = useState<File>({} as File)
+  const inputRef = useRef<HTMLInputElement>(null)
+
+  useEffect(() => {
+    setFiles((prevFiles) => prevFiles.map(
+      (prevFile) => prevFile.id === currentFile.id
+        ? currentFile
+        : { ...prevFile, active: false },
+    ))
+
+    if (currentFile.status !== 'saved') {
+      const savingStatusTimeoutId = setTimeout(() => {
+        setCurrentFile((prevCurrentFile) => ({
+          ...prevCurrentFile,
+          status: 'saving',
+        }))
+
+        const savedStatusTimeoutId = setTimeout(() => {
+          setCurrentFile((prevCurrentFile) => ({
+            ...prevCurrentFile,
+            status: 'saved',
+          }))
+          clearTimeout(savedStatusTimeoutId)
+        }, 300)
+      }, 300)
+
+      return () => clearTimeout(savingStatusTimeoutId)
+    }
+  }, [currentFile])
+
+  const handleAddNewFile = () => {
+    const newFile: File = {
+      id: uuidv4(),
+      name: 'Sem título',
+      content: '',
+      active: true,
+      status: 'saved',
+    }
+
+    inputRef.current?.focus()
+    setFiles((prevFiles) => {
+      const prevFilesDisabled = prevFiles.map((prevFile) => ({
+        ...prevFile,
+        active: false,
+      }))
+      return [...prevFilesDisabled, newFile]
+    })
+    setCurrentFile(newFile)
+  }
+
+  const changeCurrentFilename = (newFilename: string) => {
+    setCurrentFile((prevCurrentFile) => ({
+      ...prevCurrentFile,
+      name: newFilename,
+      status: 'editing',
+    }))
+  }
+
+  const changeCurrentContent = (newContent: string) => {
+    setCurrentFile((prevCurrentFile) => ({
+      ...prevCurrentFile,
+      content: newContent,
+      status: 'editing',
+    }))
+  }
+
+  const changeFileById = (id: string) => {
+    const newCurrentFile = files.find((file) => file.id === id)
+    if (!newCurrentFile) return
+
+    inputRef.current?.focus()
+    setCurrentFile({ ...newCurrentFile, active: true })
+  }
+
+  const deleteFileById = (id: string) => {
+    setFiles((prevFiles) => prevFiles.filter(
+      (prevFile) => prevFile.id !== id,
+    ))
+  }
+
+  return {
+    files,
+    currentFile,
+    inputRef,
+    handleAddNewFile,
+    changeCurrentFilename,
+    changeCurrentContent,
+    changeFileById,
+    deleteFileById,
+  }
+}
